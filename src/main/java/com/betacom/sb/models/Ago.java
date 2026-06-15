230df9bda9124158177eacf78453e51e970ea1e1
Trafalgar-prime <kevin97.lorenzo@gmail.com>
2026-06-15 15:14:44 +0200

    second push

--- src/main/java/com/betacom/sb/models/Ago.java
+++ src/main/java/com/betacom/sb/models/Ago.java
@@ -2,4 +2,5 @@ package com.betacom.sb.models;
 
 public class Ago {
 	//test branch ago
+	//mi dvierto
 }
--- src/main/java/com/betacom/sb/models/Alex.java
+++ src/main/java/com/betacom/sb/models/Alex.java
@@ -3,4 +3,5 @@ package com.betacom.sb.models;
 public class Alex {
 	// Scusate per il branch col nome del cavolo
 	// SISTEMATO
+	//non piu
 }
