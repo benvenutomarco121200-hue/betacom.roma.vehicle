0c44815cf5b645ecbda5b7d4883dc8a9ddfc5932
Alexander <alexander111002@gmail.com>
2026-06-17 14:34:14 +0200

    feature: added checks for incorrect enum values

--- src/main/java/com/betacom/sb/enums/FuelType.java
+++ src/main/java/com/betacom/sb/enums/FuelType.java
@@ -1,10 +1,28 @@
 package com.betacom.sb.enums;
 
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import exceptions.BetacomRomaException;
+
 public enum FuelType {
 	LPG, 
 	DIESEL, 
 	PETROL, 
 	HYBRID, 
 	ELECTRIC, 
-	NONE
+	NONE;
+	
+	@JsonCreator
+    public static FuelType fromString(String value) {
+        if (value == null || value.trim().isEmpty()) {
+            return null; 
+        }
+        try {
+            return FuelType.valueOf(value.toUpperCase().trim());
+        } catch (IllegalArgumentException e) {
+            throw new BetacomRomaException("The value '" + value + "' is not a valid FuelType. Accepted values: " + Arrays.toString(FuelType.values()));
+        }
+    }
 }
